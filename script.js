eb419f2f6d734e1be4487e388c02f4cca10e79e6
CHAOUCHI <massi.chaouchi@gmail.com>
2023-02-05 01:20:00 +0100

    Edit CSS for the BEM principles
     + image fit to container div
     + made the css variables gloables to all the html document

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const html = document.documentElement;
 const div = document.querySelector("div");
 const div_width = div.offsetWidth;
 /** Angle max que va accomplir ma div lors de la rotation*/
@@ -8,23 +8,23 @@ let angleDestination = 45;
  * Lorsque la souris bouge
  */
 document.documentElement.addEventListener("mousemove",(e)=>{
-    /**
-     * Je concaténe le resultat de float y(x)
-     * comme ceci
-     * "ydeg";
-     */
-    div.style.setProperty("--var-angle",`${y(e.clientX)}deg`)
-
-    /**
-     * TODO
-     * J'essaye de faire bouger le vecteur de direction 
-     * de l'angle avec le curseur de la souris
-     * Problème : les varible css --var-x et --var-y ne semble pas 
-     * se mettre à jours correctement
-     */
-    const mousePos = getMousePosition(e);
-    div.style.setProperty("--var-x",`${mousePos.x}`,"important")
-    div.style.setProperty("--var-y",`${mousePos.x}`,"important")
+    
+        /**
+         * Je concaténe le resultat de float y(x)
+         * comme ceci
+         * "ydeg";
+         */
+        html.style.setProperty("--var-angle",`${y(e.clientX)}deg`)
+        /**
+         * TODO
+         * J'essaye de faire bouger le vecteur de direction 
+         * de l'angle avec le curseur de la souris
+         * Problème : les varible css --var-x et --var-y ne semble pas 
+         * se mettre à jours correctement
+         */
+        const mousePos = getMousePosition(e);
+        html.style.setProperty("--var-x",`${mousePos.x}`)
+        html.style.setProperty("--var-y",`${mousePos.x}`)
 });
 
 function y(x){
@@ -48,6 +48,5 @@ function getMousePosition(e){
         x : parseFloat((x/(window.innerWidth/2)).toFixed(2)),
         y : parseFloat((y/(window.innerHeight/2)).toFixed(2))
     }
-    console.log(finalPos);
     return finalPos;
 }
